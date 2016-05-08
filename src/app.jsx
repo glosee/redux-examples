a64fee047c8e35405d4e053d1d7a55abb84f95aa
Graham Losee <graham.losee@loblaw.ca>
2016-05-08 13:06:30 -0400

    Initializer

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function render() {
+	ReactDOM.render(
+		<div>Hello, world!</div>,
+		document.getElementById('root')
+	);
+};
+
+render();
